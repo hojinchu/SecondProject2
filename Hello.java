43e7d95b6ab86fb77206399edfa50c4e2fa82bfd
hojinchu <hojinchu99@gmail.com>
2015-10-06 17:08:27 +0900

    GitHub for git pull

--- Hello.java
+++ Hello.java
@@ -7,6 +7,7 @@ public class Hello{
 		System.out.println("What is Git?");
 		System.out.println("That's DVCS");
 		System.out.println("BitKeeper");
+		System.out.println("That's version Contron");
 	}
 }
 
